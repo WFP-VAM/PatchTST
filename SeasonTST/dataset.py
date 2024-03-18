6318d6bad24eaa56c7e654701c754d50d2780fe9
Paolo Lucchino <paololucchino@gmail.com>
2024-03-18 10:26:04 +0100

    Add linting to env and run lint

--- SeasonTST/dataset.py
+++ SeasonTST/dataset.py
@@ -1,9 +1,20 @@
 import numpy
-from torch.utils.data import Dataset
 from sklearn.preprocessing import StandardScaler
+from torch.utils.data import Dataset
+
 
 class Rain_Ndvi_Dataset(Dataset):
-    def __init__(self, ndvi_array, rfh_array,time_array, lat_index,lon_index, size=None, split='train', scale=True):
+    def __init__(
+        self,
+        ndvi_array,
+        rfh_array,
+        time_array,
+        lat_index,
+        lon_index,
+        size=None,
+        split="train",
+        scale=True,
+    ):
         if size is None:
             self.seq_len = 30
             self.label_len = 10
@@ -11,7 +22,7 @@ class Rain_Ndvi_Dataset(Dataset):
         else:
             self.seq_len, self.label_len, self.pred_len = size
 
-        assert split in ['train', 'val', 'test']
+        assert split in ["train", "val", "test"]
         self.split = split
         self.scale = scale
         # self.ndvi_xarray = ndvi_xarray
@@ -19,10 +30,9 @@ class Rain_Ndvi_Dataset(Dataset):
         self.ndvi_array = ndvi_array
         self.rfh_array = rfh_array
         self.time_array = time_array
-        self.features = ['rfh', 'ndvi']
+        self.features = ["rfh", "ndvi"]
         self.lat_index = lat_index
         self.lon_index = lon_index
-
 
         self.initialize_data_for_epoch()
 
@@ -34,7 +44,7 @@ class Rain_Ndvi_Dataset(Dataset):
         #     lat, lon = self.select_random_pixel()
         # Generate DataFrame for the selected pixel
         if self.split == "test":
-            print("(lat, lon) selected for test:",  (lat, lon))
+            print("(lat, lon) selected for test:", (lat, lon))
 
         self.dataframe = self.generate_pixel_dataframe(lat, lon)
         # Read and split data
@@ -46,22 +56,21 @@ class Rain_Ndvi_Dataset(Dataset):
         return lat, lon
 
     def generate_pixel_dataframe(self, lat, lon):
-        ndvi_df = pd.DataFrame(self.ndvi_array[:,lat,lon], columns=['band'])
+        ndvi_df = pd.DataFrame(self.ndvi_array[:, lat, lon], columns=["band"])
         time_values = self.time_array
         ndvi_df = ndvi_df.reset_index()
-        ndvi_df['time'] = time_values
-        ndvi = ndvi_df[['time', 'band']]
-        ndvi.rename(columns={'band': 'ndvi'}, inplace=True)
-        ndvi.set_index('time', inplace=True)
+        ndvi_df["time"] = time_values
+        ndvi = ndvi_df[["time", "band"]]
+        ndvi.rename(columns={"band": "ndvi"}, inplace=True)
+        ndvi.set_index("time", inplace=True)
 
-
-        rfh_df = pd.DataFrame(self.rfh_array[:,lat,lon], columns=['band'])
+        rfh_df = pd.DataFrame(self.rfh_array[:, lat, lon], columns=["band"])
         time_values = self.time_array
         rfh_df = rfh_df.reset_index()
-        rfh_df['time'] = time_values
-        rfh = rfh_df[['time', 'band']]
-        rfh.rename(columns={'band': 'rfh'}, inplace=True)
-        rfh.set_index('time', inplace=True)
+        rfh_df["time"] = time_values
+        rfh = rfh_df[["time", "band"]]
+        rfh.rename(columns={"band": "rfh"}, inplace=True)
+        rfh.set_index("time", inplace=True)
         df = pd.concat([rfh, ndvi], axis=1)
         return df
 
@@ -72,22 +81,23 @@ class Rain_Ndvi_Dataset(Dataset):
             self.scaler = StandardScaler()
             df[self.features] = self.scaler.fit_transform(df[self.features])
 
-
         train_size = int(0.7 * len(df))
         val_size = int(0.15 * len(df))
 
-        if self.split == 'train':
+        if self.split == "train":
             self.data = df.iloc[:train_size]
-        elif self.split == 'val':
-            self.data = df.iloc[train_size:train_size + val_size]
+        elif self.split == "val":
+            self.data = df.iloc[train_size : train_size + val_size]
         else:
-            self.data = df.iloc[train_size + val_size:]
+            self.data = df.iloc[train_size + val_size :]
 
     def __getitem__(self, index):
         # Choose a random start index for the sequence
         max_start_index = len(self.data) - self.seq_len - self.pred_len
         if max_start_index < 1:
-            raise ValueError("Dataset is too small for the specified sequence and prediction lengths.")
+            raise ValueError(
+                "Dataset is too small for the specified sequence and prediction lengths."
+            )
         random_start = np.random.randint(0, max_start_index)
 
         s_begin = index
@@ -95,13 +105,12 @@ class Rain_Ndvi_Dataset(Dataset):
         r_begin = s_end
         r_end = r_begin + self.pred_len
 
-
         seq_x = self.data.iloc[s_begin:s_end][self.features].values
         seq_y = self.data.iloc[r_begin:r_end][self.features].values
 
-
-        return torch.tensor(seq_x, dtype=torch.float32), torch.tensor(seq_y, dtype=torch.float32)
-
+        return torch.tensor(seq_x, dtype=torch.float32), torch.tensor(
+            seq_y, dtype=torch.float32
+        )
 
     def __len__(self):
         return len(self.data) - self.seq_len - self.pred_len + 1
@@ -110,8 +119,6 @@ class Rain_Ndvi_Dataset(Dataset):
         if self.scale:
             return self.scaler.inverse_transform(data)
         return data
-
-
 
     def __len__(self):
         return len(self.data) - self.seq_len - self.pred_len + 1
--- SeasonTST/utils.py
+++ SeasonTST/utils.py
@@ -1,3 +1,8 @@
+from types import SimpleNamespace
+
+import xarray as xr
+from torch.utils.data import Dataset
+
 from PatchTST_self_supervised.src.callback.patch_mask import PatchMaskCB
 from PatchTST_self_supervised.src.callback.transforms import RevInCB
 from PatchTST_self_supervised.src.data.datamodule import DataLoaders
@@ -5,64 +10,65 @@ from PatchTST_self_supervised.src.learner import Learner
 from PatchTST_self_supervised.src.models.patchTST import PatchTST
 from SeasonTST.dataset import Rain_Ndvi_Dataset
 
-from types import SimpleNamespace
-
-import xarray as xr
-from torch.utils.data import Dataset
-
 
 def get_dataloaders(
-        config_obj: SimpleNamespace,
-        dataset_class: Dataset,
-        dataset: xr.Dataset,
-        lat: float =  0.0,
-        lon: float = 0.0
+    config_obj: SimpleNamespace,
+    dataset_class: Dataset,
+    dataset: xr.Dataset,
+    lat: float = 0.0,
+    lon: float = 0.0,
 ):
     size = [config_obj.sequence_length, 0, config_obj.prediction_length]
     dls = DataLoaders(
-            datasetCls=dataset_class,
-            dataset_kwargs={
-                'ndvi_array':ndvi_array ,
-                'rfh_array':rfh_array ,
-                'time_array':time_array ,
-                'size':size,
-                'scale':True,
-                'lat_index': lat,
-                'lon_index': lon
-            },
-            batch_size=config_obj.batch_size,
-            workers=config_obj.num_workers,
-            )
+        datasetCls=dataset_class,
+        dataset_kwargs={
+            "ndvi_array": ndvi_array,
+            "rfh_array": rfh_array,
+            "time_array": time_array,
+            "size": size,
+            "scale": True,
+            "lat_index": lat,
+            "lon_index": lon,
+        },
+        batch_size=config_obj.batch_size,
+        workers=config_obj.num_workers,
+    )
 
     dls.vars, dls.len = dls.train.dataset[0][0].shape[1], config_obj.sequence_length
     dls.c = dls.train.dataset[0][1].shape[0]
     return dls
 
 
-def get_model(config, headtype='pretrain'):
-    stride  = config.stride
+def get_model(config, headtype="pretrain"):
+    stride = config.stride
     # get number of patches
-    num_patch = (max(config.sequence_length, config.patch_len)-config.patch_len) // stride + 1
+    num_patch = (
+        max(config.sequence_length, config.patch_len) - config.patch_len
+    ) // stride + 1
 
     # get model
-    model = PatchTST(c_in=config.c_in,
-                target_dim=config.prediction_length,
-                patch_len=config.patch_len,
-                stride=stride,
-                num_patch=num_patch,
-                n_layers=16, #number of Transformer layers
-                n_heads=64,#number of Transformer heads
-                d_model=128, #Transformer d_model
-                shared_embedding=True,
-                d_ff=512, #Tranformer MLP dimension
-                dropout=2e-1, #Transformer dropout
-                head_dropout=2e-1, #head dropout
-                act='relu',
-                head_type=headtype,
-                res_attention=False
-                )
+    model = PatchTST(
+        c_in=config.c_in,
+        target_dim=config.prediction_length,
+        patch_len=config.patch_len,
+        stride=stride,
+        num_patch=num_patch,
+        n_layers=16,  # number of Transformer layers
+        n_heads=64,  # number of Transformer heads
+        d_model=128,  # Transformer d_model
+        shared_embedding=True,
+        d_ff=512,  # Tranformer MLP dimension
+        dropout=2e-1,  # Transformer dropout
+        head_dropout=2e-1,  # head dropout
+        act="relu",
+        head_type=headtype,
+        res_attention=False,
+    )
     # print out the model size
-    print('number of model params', sum(p.numel() for p in model.parameters() if p.requires_grad))
+    print(
+        "number of model params",
+        sum(p.numel() for p in model.parameters() if p.requires_grad),
+    )
     return model
 
 
@@ -80,18 +86,26 @@ def find_learning_rate(config_obj):
     dls = get_dataloaders(config_obj, Rain_Ndvi_Dataset)
     model = get_model(config_obj)
     # get loss
-    loss_func = torch.nn.MSELoss(reduction='mean')
+    loss_func = torch.nn.MSELoss(reduction="mean")
     # get callbacks
     cbs = [RevInCB(dls.vars, denorm=False)] if config_obj.revin else []
-    cbs += [PatchMaskCB(patch_len=config_obj.patch_len, stride=config_obj.stride, mask_ratio=config_obj.mask_ratio)]
+    cbs += [
+        PatchMaskCB(
+            patch_len=config_obj.patch_len,
+            stride=config_obj.stride,
+            mask_ratio=config_obj.mask_ratio,
+        )
+    ]
 
     # define learner
-    learn = Learner(dls, model,
-                        loss_func,
-                        lr=config_obj.lr,
-                        cbs=cbs,
-                        )
+    learn = Learner(
+        dls,
+        model,
+        loss_func,
+        lr=config_obj.lr,
+        cbs=cbs,
+    )
     # fit the data to the model
     suggested_lr = learn.lr_finder()
-    print('suggested_lr', suggested_lr)
+    print("suggested_lr", suggested_lr)
     return suggested_lr
